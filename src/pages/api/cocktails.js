92d18606fe54dea18de41d1a459bedf3926a7e91
Niklas Ostwald <niktjawalla922@gmail.com>
2022-09-29 14:57:26 +0200

    fix error, create new object model for api

--- src/pages/api/cocktails.js
+++ src/pages/api/cocktails.js
@@ -3,6 +3,17 @@ const numbers = Array.from({length: 26}).map((_, i) => {
 });
 const alphabet = numbers.map(number => String.fromCharCode(number));
 
+const ingredientPrefix = 'strIngredient';
+const ingredientCount = 15;
+const ingredientKeys = Array.from({length: ingredientCount}).map((_, index) => {
+	return `${ingredientPrefix}${index + 1}`;
+});
+
+const measurementPrefix = 'strMeasure';
+const measureKeys = Array.from({length: 15}).map((_, index) => {
+	return `${measurementPrefix}${index + 1}`;
+});
+
 export default async function handler(request, response) {
 	const objects = await Promise.all(
 		alphabet.map(async letter => {
@@ -14,11 +25,28 @@ export default async function handler(request, response) {
 		})
 	);
 	const drinksA_Z = objects.map(object => object.drinks);
-	const drinks = drinksA_Z.flat(1);
+	const drinks = drinksA_Z.flat(1).filter(drink => drink !== null);
 
-	drinks.map(drink => {
-		console.log(drink.strDrink);
+	const cocktails = drinks.map(drink => {
+		const nullIngredients = ingredientKeys.map(key => {
+			return drink[key];
+		});
+		const nullMeasurements = measureKeys.map(key => {
+			return drink[key];
+		});
+		const ingredients = nullIngredients.filter(ingredient => ingredient !== null);
+		const measurements = nullMeasurements.filter(measurement => measurement !== null);
+		return {
+			id: drink.idDrink,
+			name: drink.strDrink,
+			ingredients: {
+				names: ingredients,
+				measurements: measurements,
+			},
+			instructions: drink.strInstructions,
+			image: drink.strDrinkThumb,
+		};
 	});
 
-	response.status(200).json(drinks);
+	response.status(200).json(cocktails);
 }
