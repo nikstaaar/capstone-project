152fbc3d763076c7891ca859766c04f4a21a101a
Your Name <niktjawalla922@gmail.com>
2024-01-05 16:07:05 +0100

    update Put handler

--- src/pages/api/users/[userId].js
+++ src/pages/api/users/[userId].js
@@ -9,11 +9,13 @@ export default async function handler(request, response) {
 			getUser(request, response);
 			break;
 		case 'PUT':
-			await putUser(request, response);
-			response.status(200).json({method, name: 'PUT Request'});
+			try {
+				await putUser(request, response);
+
+				response.status(200).json({method, name: 'PUT Request'});
+			} catch (error) {
+				response.status(500).json({error: 'Error processing PUT request'});
+			}
 			break;
-		default:
-			response.setHeader('Allow', ['GET', 'PUT', 'DEL']);
-			response.status(405).end(`Method${method} Not Allowed`);
 	}
 }
