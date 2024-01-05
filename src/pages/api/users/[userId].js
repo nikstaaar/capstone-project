50fa82ba469c6fc60aed023dd33eeea5f3cfe197
Your Name <niktjawalla922@gmail.com>
2024-01-05 15:35:11 +0100

    fix user id error

--- src/pages/api/users/[userId].js
+++ src/pages/api/users/[userId].js
@@ -2,36 +2,18 @@ import {getUser, putUser} from '../../../backend/lib/controller';
 import connectMongo from '../../../backend/lib/dbConnect';
 
 export default async function handler(request, response) {
-	try {
-		await connectMongo();
-
-		const {method} = request;
-		switch (method) {
-			case 'GET':
-				getUser(request, response);
-				break;
-			case 'PUT':
-				await updateUser(request, response);
-				break;
-			default:
-				response.setHeader('Allow', ['GET', 'PUT', 'DEL']);
-				response.status(405).end(`Method ${method} Not Allowed`);
-		}
-	} catch (error) {
-		response.status(500).json({error: 'Internal Server Error'});
-	}
-}
-
-async function updateUser(request, response) {
-	try {
-		const {userId} = request.query;
-		const data = request.body;
-		const filter = {email: userId};
-		const update = {ingredients: data};
-
-		await putUser(filter, update);
-		response.status(200).json({message: 'User updated successfully'});
-	} catch (error) {
-		response.status(404).json({error: 'Error while updating the Data'});
+	connectMongo().catch(() => response.status(405).json({error: 'Error in the Connection'}));
+	const {method} = request;
+	switch (method) {
+		case 'GET':
+			getUser(request, response);
+			break;
+		case 'PUT':
+			await putUser(request, response);
+			response.status(200).json({method, name: 'PUT Request'});
+			break;
+		default:
+			response.setHeader('Allow', ['GET', 'PUT', 'DEL']);
+			response.status(405).end(`Method${method} Not Allowed`);
 	}
 }
